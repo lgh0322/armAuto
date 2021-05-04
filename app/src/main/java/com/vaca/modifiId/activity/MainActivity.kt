d9eead85ced4ad3fbb185ad918976fcf12d6e1f6
李光浩 <liguanghao@viatomtech.com>
2021-05-04 21:11:44 +0800

    ad5

--- app/src/main/java/com/vaca/modifiId/activity/MainActivity.kt
+++ app/src/main/java/com/vaca/modifiId/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.vaca.modifiId.activity
 
+import android.app.Application
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.content.Context
@@ -9,20 +11,63 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.vaca.modifiId.BleServer
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.GridLayoutManager
+
+import com.vaca.modifiId.adapter.BleViewAdapter
+import com.vaca.modifiId.bean.BleBean
+import com.vaca.modifiId.ble.BleDataWorker
+import com.vaca.modifiId.ble.BleScanManager
 import com.vaca.modifiId.databinding.ActivityMainBinding
 import com.vaca.modifiId.utils.InputFilterMinMax
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),BleViewAdapter.ItemClickListener {
     lateinit var x1: EditText
     lateinit var x2: EditText
     lateinit var x3: EditText
     lateinit var x4: EditText
+    private val bleList: MutableList<BleBean> = ArrayList()
+    var nrfConnect = false
 
+    val dataScope = CoroutineScope(Dispatchers.IO)
+    val BLE_DATA_WORKER: BleDataWorker = BleDataWorker()
+    val scan = BleScanManager()
+    val mainVisible=MutableLiveData<Boolean>()
+
+
+    lateinit var bleViewAdapter : BleViewAdapter
+
+    fun updateDevice(byteArray: ByteArray) {
+        dataScope.launch {
+            BLE_DATA_WORKER.updateDevice(byteArray)
+        }
+
+    }
+
+    fun setScan(bluetoothLeScanner: BluetoothLeScanner) {
+        scan.setScan(bluetoothLeScanner)
+    }
+
+
+    fun startServer(app: Application) {
+        scan.start()
+        scan.setCallBack(object : BleScanManager.Scan {
+            override fun scanReturn(name: String, bluetoothDevice: BluetoothDevice) {
+                var z: Int = 0;
+                for (ble in bleList) run {
+                    if (ble.name == bluetoothDevice.name) {
+                        z = 1
+                    }
+                }
+                if (z == 0) {
+                    bleList.add(BleBean(name, bluetoothDevice))
+                    bleViewAdapter.addDevice(name, bluetoothDevice)
+                }
+            }
+        })
+    }
 
     lateinit var binding: ActivityMainBinding
 
@@ -52,7 +97,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_ENABLE_BT) {
-            BleServer.dataScope.launch {
+           dataScope.launch {
                 bleChannel.send(true)
             }
 
@@ -69,16 +114,28 @@ class MainActivity : AppCompatActivity() {
         binding.x3.filters = arrayOf(InputFilterMinMax("0", "255"))
         binding.x4.filters = arrayOf(InputFilterMinMax("0", "255"))
 
+        bleViewAdapter = BleViewAdapter(this)
+        binding.bleTable.adapter=bleViewAdapter
+        binding.bleTable.layoutManager= GridLayoutManager(this, 2);
+        bleViewAdapter.setClickListener(this)
 
 
+        mainVisible.observe(this,{
+            if(it){
+                binding.group.visibility=View.VISIBLE
+                binding.bleTable.visibility=View.GONE
+            }else{
+                binding.group.visibility=View.INVISIBLE
+                binding.bleTable.visibility=View.VISIBLE
+            }
 
-
+        })
 
         Thread {
             runBlocking {
-                BleServer.setScan(getLeScan())
+               setScan(getLeScan())
             }
-            BleServer.startServer(application)
+          startServer(application)
 
 
         }.start()
@@ -87,6 +144,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun writeId(view: View) {
-        BleServer.BLE_DATA_WORKER.sendCmd(byteArrayOf(x1.text.toString().toInt().toByte(), x2.text.toString().toInt().toByte(), x3.text.toString().toInt().toByte(), x4.text.toString().toInt().toByte()))
+       BLE_DATA_WORKER.sendCmd(byteArrayOf(x1.text.toString().toInt().toByte(), x2.text.toString().toInt().toByte(), x3.text.toString().toInt().toByte(), x4.text.toString().toInt().toByte()))
+    }
+
+    override fun onScanItemClick(bluetoothDevice: BluetoothDevice?) {
+
+        BLE_DATA_WORKER.initWorker(application, bluetoothDevice)
+        mainVisible.postValue(true)
+    }
+
+
+    override fun onBackPressed() {
+        moveTaskToBack(false)
+
     }
 }
+
+
